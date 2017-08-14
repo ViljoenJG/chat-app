8f16454794067033da9f902e8e179dcc6cecac8c
Kobus Viljoen <kobusv@gmail.com>
2017-08-14 21:54:18 +0200

    scroll to bottom on new message

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeigth = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeigth + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('new-message', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('new-location-message', function(message) {
@@ -29,7 +47,8 @@ socket.on('new-location-message', function(message) {
     createdAt: formattedTime
   });
 
-  jQuery('#messages').append(html)
+  jQuery('#messages').append(html);
+  scrollToBottom();
 })
 
 jQuery('#message-form').on('submit', function(e) {
